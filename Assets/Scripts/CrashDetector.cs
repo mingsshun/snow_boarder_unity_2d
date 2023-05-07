aeffe2a9b284250236466c93335e0618de402a75
MingShun <min.shuun@gmail.com>
2023-05-07 15:23:10 +0700

    - 10 NameSpaces & SceneManagement

--- Assets/Scripts/CrashDetector.cs
+++ Assets/Scripts/CrashDetector.cs
@@ -1,12 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CrashDetector : MonoBehaviour
 {
     void OnTriggerEnter2D(Collider2D other){
         if(other.tag == "Ground"){
-            Debug.Log("Scrashhhhhh");
+            SceneManager.LoadScene(0);
         }
     }
 }
