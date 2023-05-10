a0103700b071c090a843b7f187771cc07ab6c3e5
MingShun <min.shuun@gmail.com>
2023-05-10 13:47:24 +0700

    - 13 Triggering Particle Effects

--- Assets/Scripts/CrashDetector.cs
+++ Assets/Scripts/CrashDetector.cs
@@ -6,8 +6,10 @@ using UnityEngine.SceneManagement;
 public class CrashDetector : MonoBehaviour
 {
     [SerializeField] float loadDelay = 0.5f;
+    [SerializeField] ParticleSystem crashEffect = null;
     void OnTriggerEnter2D(Collider2D other){
         if(other.tag == "Ground"){
+            crashEffect.Play();
             Invoke("ReloadScene", loadDelay);
         }
     }
--- Assets/Scripts/FinishLine.cs
+++ Assets/Scripts/FinishLine.cs
@@ -6,8 +6,10 @@ using UnityEngine.SceneManagement;
 public class FinishLine : MonoBehaviour
 {
     [SerializeField] float loadDelay = 1f;
+    [SerializeField] ParticleSystem finishEffect = null;
     void OnTriggerEnter2D(Collider2D other){
         if(other.tag == "Player"){
+            finishEffect.Play();
             Invoke("ReloadScene", loadDelay);
         }
     }
